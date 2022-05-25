779d7982c1218adc6e54398db0a63f2c5e9b3179
ivanmyzou <ivanmyzou@gmail.com>
2022-05-25 21:19:32 +1000

    GUI basic scenarios

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import tkinter as tk
 from tkinter import ttk
 from ttkthemes import ThemedTk
 
+from distribution import distributions
+
 import util
 
 
@@ -22,6 +24,12 @@ class QS(tk.Frame):
         self.dropdown1['values'] = ['M (Markovian)', 'G (General)']
         self.dropdown1.current(0) #defaults to M
         self.dropdown1.grid(row=1, column=1)
+        self.dropdown1.bind("<<ComboboxSelected>>", lambda event: self.selectMG_1(event)) #disable/enable distribution selections
+        ttk.Label(self, text = '').grid(row=1, column=2)  # padding
+        self.dis1 = ttk.Combobox(self, width = 15, textvariable = tk.StringVar(), state = "disabled")
+        self.dis1['values'] = distributions
+        self.dis1.current(0) #defaults to M
+        self.dis1.grid(row=1, column=3)
 
         ttk.Label(self, text='').grid(row=2, column=0)  # padding
         self.tag2 = ttk.Label(self, text = 'Service Workloads', width = 25,
@@ -31,6 +39,12 @@ class QS(tk.Frame):
         self.dropdown2['values'] = ['M (Markovian)', 'G (General)']
         self.dropdown2.current(0)  # defaults to M
         self.dropdown2.grid(row=3, column=1)
+        self.dropdown2.bind("<<ComboboxSelected>>", lambda event: self.selectMG_2(event)) #disable/enable distribution selections
+        ttk.Label(self, text = '').grid(row=3, column=2)  # padding
+        self.dis2 = ttk.Combobox(self, width = 15, textvariable = tk.StringVar(), state = "disabled")
+        self.dis2['values'] = distributions
+        self.dis2.current(0) #defaults to M
+        self.dis2.grid(row=3, column=3)
 
         ttk.Label(self, text='').grid(row=4, column=0)  # padding
         self.tag3 = ttk.Label(self, text = 'Number of Servers', width = 25,
@@ -40,7 +54,21 @@ class QS(tk.Frame):
         self.nserver_entry.insert(0, '1')
         self.nserver_entry.grid(row=5, column=1)
 
+    def selectMG_1(self, event):
+        MG = event.widget.get()
+        if MG == 'M (Markovian)':
+            self.dis1.current(0) #lock in M
+            self.dis1.configure(state = 'disabled')
+        else:
+            self.dis1.configure(state = 'readonly')
 
+    def selectMG_2(self, event):
+        MG = event.widget.get()
+        if MG == 'M (Markovian)':
+            self.dis2.current(0) #lock in M
+            self.dis2.configure(state = 'disabled')
+        else:
+            self.dis2.configure(state = 'readonly')
 
 if __name__ == '__main__':
     root = ThemedTk(theme="breeze")
